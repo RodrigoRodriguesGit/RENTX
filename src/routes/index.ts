7e2cc53313bd5758065bba0efc06801e673e124d
rodrigom <rodrigo.rodrigues.live@hotmail.com>
2022-07-08 13:21:20 -0300

    Study Day 08/07/2022 13:21

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import  express, { Router } from 'express'
+import { CreateUserController } from '../modules/accounts/useCases/CreateUser/CreateUserController';
 import { categoriesRoutes } from '../routes/categories.routes' 
 import { specificationsRoutes } from '../routes/specification.routes' 
+import { usersRoutes } from './users.routes';
 
 const router = Router();
 
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
+router.use("/users", usersRoutes);
 
 export { router }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,13 @@
 import { container } from "tsyringe"
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository"
-import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository"
 import { ISpecificationRepository } from "../../modules/cars/repositories/ISpecificationRepository"
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository"
+
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository"
 import { SpecificationRepository } from "../../modules/cars/repositories/implementations/SpecificationRepository"
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository"
+
 import { categoriesRoutes } from "../../routes/categories.routes"
 
 container.registerSingleton<ICategoriesRepository>(
@@ -14,4 +18,10 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
     "SpecificationRepository", 
     SpecificationRepository
+)
+
+container.registerSingleton<IUsersRepository> (
+    "UsersRepository", 
+    UsersRepository
+
 )
