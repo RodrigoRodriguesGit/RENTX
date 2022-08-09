dcb02627f0f11ad1266e6f15e10be0cc9986b3c2
rodrigom <rodrigo.rodrigues.live@hotmail.com>
2022-08-09 08:58:18 -0300

    Study day 09/08/2022 08:58

---
+++ src/shared/infra/typeorm/migrations/1660046022703-CreateCars.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCars1660046022703 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
