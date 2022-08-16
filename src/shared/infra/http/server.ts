0bfb9fd2997ad3846e2bc8d80002a84027621bca
rodrigom <rodrigo.rodrigues.live@hotmail.com>
2022-08-16 08:57:01 -0300

    Study day 16/08/2022 08:56

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors"
 import swaggerUi from 'swagger-ui-express'
-import "../typeorm";
+import createConnection from "@shared/infra/typeorm";
 import "@shared/container";
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json'
@@ -10,6 +10,7 @@ import { categoriesRoutes } from './routes/categories.routes';
 import { specificationsRoutes } from './routes/specification.routes';
 import { AppError } from "@shared/errors/AppError";
 
+createConnection()
 const app = express();
 
 app.use(express.json());
