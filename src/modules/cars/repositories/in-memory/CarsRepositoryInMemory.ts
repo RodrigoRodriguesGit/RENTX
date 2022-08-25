bb822daee9dbe050ceec7ba1a885db76bbb992d3
Rodrigo Rodrigues <rodrigo.rodrigues.live@hotmail.com>
2022-08-25 08:55:27 -0300

    Study day 25/08/2022 08:55

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -57,6 +57,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
     }
 
+    async findById(id: string): Promise<Car> {
+        return this.cars.find((car) => car.id === id)
+    }
+
 }
 
 export { CarsRepositoryInMemory }
