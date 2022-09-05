9114d49cff6026eb8bd966a6c61bec7507a30578
Rodrigo Rodrigues <rodrigo.rodrigues.live@hotmail.com>
2022-09-05 09:05:14 -0300

    Study day 05/09/2022 09:05

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -4,7 +4,7 @@ import { CreateRentalUseCase } from "./CreateRentalUseCase"
 let createRentalUseCase:  CreateRentalUseCase
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory
 
-describe("Create Rental", async () => {
+describe("Create Rental", () => {
 
     beforeEach(() => {
         rentalsRepositoryInMemory = new RentalsRepositoryInMemory()
@@ -13,11 +13,37 @@ describe("Create Rental", async () => {
 
     it("should be able to create a new rental", async () => {
         
+        const rental = await createRentalUseCase.execute({
+            user_id: "12345",
+            car_id: "121212",
+            expected_return_date: new Date()
+        })
+
+        console.log(rental)
+
+        expect(rental).toHaveProperty("id")
+        expect(rental).toHaveProperty("start_date")
+
+    }) 
+
+    it("should not be able to create a new rental if there is another open to the same user", async () => {
+
         await createRentalUseCase.execute({
             user_id: "12345",
             car_id: "121212",
             expected_return_date: new Date()
         })
+
+        const rental = await createRentalUseCase.execute({
+            user_id: "12345",
+            car_id: "121212",
+            expected_return_date: new Date()
+        })
+
+        console.log(rental)
+
+        expect(rental).toHaveProperty("id")
+        expect(rental).toHaveProperty("start_date")
 
     }) 
 
